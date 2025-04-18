36e0228de616c7dc3f6331c7b5d68b2f0c3c64ea
Samuel Adrian Cerezo <samuel_cerezo@icloud.com>
2025-04-18 14:23:03 +0200

    links updated

--- download_data.py
+++ download_data.py
@@ -4,17 +4,53 @@ import requests
 from tqdm import tqdm
 import zipfile
 
-
-
-# TO BE MODIFIED
-
 SEQUENCES = {
-    "setup-1-warm-train": "https://zenodo.org/record/.../files/1-warm-train.zip?download=1",
-    "setup-1-warm-test": "https://zenodo.org/record/.../files/1-warm-test.zip?download=1",
-    "setup-1-cold-train": "https://zenodo.org/record/.../files/1-cold-train.zip?download=1",
-    # ...
-    "setup-5-dark-test": "https://zenodo.org/record/.../files/5-dark-test.zip?download=1",
+    "setup-1-natural-train": "https://zenodo.org/records/15000694/files/1-natural-tr.zip?download=1",
+    "setup-1-natural-test": "https://zenodo.org/records/15000694/files/1-natural-tt.zip?download=1",
+    "setup-1-dark-train": "https://zenodo.org/records/15000694/files/1-dark-tr.zip?download=1",
+    "setup-1-dark-test": "https://zenodo.org/records/15000694/files/1-dark-tt.zip?download=1",
+    "setup-1-warm-train": "https://zenodo.org/records/15000694/files/1-warm-tr.zip?download=1",
+    "setup-1-warm-test": "https://zenodo.org/records/15000694/files/1-warm-tt.zip?download=1",
+    "setup-1-cold-train": "https://zenodo.org/records/15000694/files/1-cold-tr.zip?download=1",
+    "setup-1-cold-test": "https://zenodo.org/records/15000694/files/1-cold-tt.zip?download=1",
+
+    "setup-2-natural-train": "https://zenodo.org/records/15000694/files/2-natural-tr.zip?download=1",
+    "setup-2-natural-test": "https://zenodo.org/records/15000694/files/2-natural-tt.zip?download=1",
+    "setup-2-dark-train": "https://zenodo.org/records/15000694/files/2-dark-tr.zip?download=1",
+    "setup-2-dark-test": "https://zenodo.org/records/15000694/files/2-dark-tt.zip?download=1",
+    "setup-2-warm-train": "https://zenodo.org/records/15000694/files/2-warm-tr.zip?download=1",
+    "setup-2-warm-test": "https://zenodo.org/records/15000694/files/2-warm-tt.zip?download=1",
+    "setup-2-cold-train": "https://zenodo.org/records/15000694/files/2-cold-tr.zip?download=1",
+    "setup-2-cold-test": "https://zenodo.org/records/15000694/files/2-cold-tt.zip?download=1",
+
+    "setup-3-natural-train": "https://zenodo.org/records/15000694/files/3-natural-tr.zip?download=1",
+    "setup-3-natural-test": "https://zenodo.org/records/15000694/files/3-natural-tt.zip?download=1",
+    "setup-3-dark-train": "https://zenodo.org/records/15000694/files/3-dark-tr.zip?download=1",
+    "setup-3-dark-test": "https://zenodo.org/records/15000694/files/3-dark-tt.zip?download=1",
+    "setup-3-warm-train": "https://zenodo.org/records/15000694/files/3-warm-tr.zip?download=1",
+    "setup-3-warm-test": "https://zenodo.org/records/15000694/files/3-warm-tt.zip?download=1",
+    "setup-3-cold-train": "https://zenodo.org/records/15000694/files/3-cold-tr.zip?download=1",
+    "setup-3-cold-test": "https://zenodo.org/records/15000694/files/3-cold-tt.zip?download=1",
+
+    "setup-4-natural-train": "https://zenodo.org/records/15000939/files/4-natural-tr.zip?download=1",
+    "setup-4-natural-test": "https://zenodo.org/records/15000939/files/4-natural-tt.zip?download=1",
+    "setup-4-dark-train": "https://zenodo.org/records/15000939/files/4-dark-tr.zip?download=1",
+    "setup-4-dark-test": "https://zenodo.org/records/15000939/files/4-dark-tt.zip?download=1",
+    "setup-4-warm-train": "https://zenodo.org/records/15000939/files/4-warm-tr.zip?download=1",
+    "setup-4-warm-test": "https://zenodo.org/records/15000939/files/4-warm-tt.zip?download=1",
+    "setup-4-cold-train": "https://zenodo.org/records/15000939/files/4-cold-tr.zip?download=1",
+    "setup-4-cold-test": "https://zenodo.org/records/15000939/files/4-cold-tt.zip?download=1",
+
+    "setup-5-natural-train": "https://zenodo.org/records/15000939/files/5-natural-tr.zip?download=1",
+    "setup-5-natural-test": "https://zenodo.org/records/15000939/files/5-natural-tt.zip?download=1",
+    "setup-5-dark-train": "https://zenodo.org/records/15000939/files/5-dark-tr.zip?download=1",
+    "setup-5-dark-test": "https://zenodo.org/records/15000939/files/5-dark-tt.zip?download=1",
+    "setup-5-warm-train": "https://zenodo.org/records/15000939/files/5-warm-tr.zip?download=1",
+    "setup-5-warm-test": "https://zenodo.org/records/15000939/files/5-warm-tt.zip?download=1",
+    "setup-5-cold-train": "https://zenodo.org/records/15000939/files/5-cold-tr.zip?download=1",
+    "setup-5-cold-test": "https://zenodo.org/records/15000939/files/5-cold-tt.zip?download=1",
 }
+
 
 def download_file(url, dest_path):
     response = requests.get(url, stream=True)
